03c7040542814d71868f400e9f50aa3e7d2a12dd
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-02-28 15:37:50 +0200

    Added view & serializer for book model

--- library_service/urls.py
+++ library_service/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/library/", include("book.urls", namespace="library")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
