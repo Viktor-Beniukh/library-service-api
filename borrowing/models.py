08a781007f9bf738a06ab19a5fd365ae06278a7b
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-02-28 20:58:48 +0200

    Added borrowing model and migration

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,34 @@
+from datetime import date, timedelta
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+
+def default_return_expecting_date():
+    return date.today() + timedelta(days=1)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(
+        default=default_return_expecting_date()
+    )
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    borrower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ("borrow_date",)
+
+    def __str__(self):
+        return f"{self.borrow_date}"
