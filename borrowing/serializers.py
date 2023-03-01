c4beb2a6de4136092a9febe898c7d154ee8b949e
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-01 22:23:18 +0200

    Added post method to borrowing endpoint with manage inventory

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -20,5 +20,41 @@ class BorrowingSerializer(serializers.ModelSerializer):
 
 
 class BorrowingListSerializer(BorrowingSerializer):
+    book_title = serializers.CharField(
+        source="book.title", read_only=True
+    )
+    book_inventory = serializers.IntegerField(
+        source="book.inventory", read_only=True
+    )
+    borrower_full_name = serializers.CharField(
+        source="borrower.full_name",
+        read_only=True
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book_title",
+            "book_inventory",
+            "borrower_full_name",
+        )
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
     book = BookSerializer(many=False, read_only=True)
     borrower = UserSerializer(many=False, read_only=True)
+
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+
+    def create(self, validated_data):
+        borrowing = Borrowing.objects.create(**validated_data)
+        if borrowing.borrow_date:
+            borrowing.book.inventory -= 1
+            borrowing.book.save()
+            borrowing.save()
+            return borrowing
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,18 +1,28 @@
 from rest_framework import viewsets
 
-from book.permissions import IsAdminOrIfAllowAnyReadOnly
+
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
+)
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "borrower")
     serializer_class = BorrowingSerializer
-    permission_classes = (IsAdminOrIfAllowAnyReadOnly,)
 
     def get_serializer_class(self):
 
-        if self.action in ("list", "retrieve"):
+        if self.action == "list":
             return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        if self.action == "create":
+            return BorrowingCreateSerializer
 
         return BorrowingSerializer
