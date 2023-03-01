e1f0c54df238fab7d6abc2e5468977505c69d9ff
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-01 17:51:44 +0200

    Refactoring permissions.py

--- book/permissions.py
+++ book/permissions.py
@@ -1,14 +1,9 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAnonAndAuthenticatedReadOnly(BasePermission):
+class IsAdminOrIfAllowAnyReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS or
-            (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            )
+            request.method in SAFE_METHODS
             or (request.user and request.user.is_staff)
         )
--- book/views.py
+++ book/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 
 from book.models import Book
-from book.permissions import IsAdminOrIfAnonAndAuthenticatedReadOnly
+from book.permissions import IsAdminOrIfAllowAnyReadOnly
 from book.serializers import BookSerializer
 
 
-class BookPagination(PageNumberPagination):
+class LibraryPagination(PageNumberPagination):
     page_size = 5
     max_page_size = 100
 
@@ -14,5 +14,5 @@ class BookPagination(PageNumberPagination):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    pagination_class = BookPagination
-    permission_classes = (IsAdminOrIfAnonAndAuthenticatedReadOnly,)
+    pagination_class = LibraryPagination
+    permission_classes = (IsAdminOrIfAllowAnyReadOnly,)
