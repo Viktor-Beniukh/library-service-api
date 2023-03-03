05a2750b75d5fbba8b77244101192f584b0e61d7
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-03 09:44:26 +0200

    Added restrictions on access to information for non-admins

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
+
+from book.permissions import IsAdminOrIfAuthenticatedReadOnly
+from book.views import LibraryPagination
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -11,17 +13,14 @@ from borrowing.serializers import (
 )
 
 
-class LibraryPagination(PageNumberPagination):
-    page_size = 5
-    max_page_size = 100
-
-
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "borrower")
     serializer_class = BorrowingSerializer
     pagination_class = LibraryPagination
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
+        status = self.request.user.is_staff
         user_id_str = self.request.query_params.get("user_id")
         is_active = self.request.query_params.get("is_active")
 
@@ -32,6 +31,9 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
         if is_active:
             queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if status is False:
+            queryset = queryset.filter(borrower=self.request.user)
 
         return queryset
 
