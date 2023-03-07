d7dac8ff95b56d37cd5a49983195693453276ead
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-07 18:51:45 +0200

    Implemented a daily-based function for checking borrowings overdue.

--- book/notifications.py
+++ book/notifications.py
@@ -32,17 +32,22 @@ def send_overdue_borrowings_notification():
 
     message = "The following borrowings are overdue:\n\n"
     for borrowing in overdue_borrowings:
-        book = borrowing.book
-        days_borrowed = (
-            borrowing.expected_return_date - borrowing.borrow_date
-        ).days
-        borrowing_amount = book.daily_fee * days_borrowed
 
-        message += f"Borrower Name: {borrowing.borrower.first_name} " \
-                   f"{borrowing.borrower.last_name}\n" \
-                   f"Amount: ${borrowing_amount}\nDue Date: " \
-                   f"{borrowing.expected_return_date}\n" \
-                   f"The fine is twice the fixed " \
-                   f"daily fee for each overdue day!\n\n"
+        if borrowing:
+            book = borrowing.book
+            days_borrowed = (
+                borrowing.expected_return_date - borrowing.borrow_date
+            ).days
+            borrowing_amount = book.daily_fee * days_borrowed
+
+            message += f"Borrower Name: {borrowing.borrower.first_name} " \
+                       f"{borrowing.borrower.last_name}\n" \
+                       f"Amount: ${borrowing_amount}\nDue Date: " \
+                       f"{borrowing.expected_return_date}\n" \
+                       f"The fine is twice the fixed " \
+                       f"daily fee for each overdue day!\n\n"
+
+        else:
+            message += "No borrowings overdue today!"
 
     bot.send_message(chat_id=settings.TELEGRAM_CHAT_ID, text=message)
