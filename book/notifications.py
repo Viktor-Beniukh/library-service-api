79c8c7e75fc0d49da8a118205cd88d914dff6cb0
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-15 19:19:27 +0200

    Fixed notifications.py

--- book/notifications.py
+++ book/notifications.py
@@ -53,51 +53,6 @@ def send_overdue_borrowings_notification():
     bot.send_message(chat_id=settings.TELEGRAM_CHAT_ID, text=message)
 
 
-def send_billing_notification(borrowing_id):
-
-    borrowing = Borrowing.objects.get(id=borrowing_id)
-    book = borrowing.book
-    days_borrowed = (
-        borrowing.expected_return_date - borrowing.borrow_date
-    ).days
-
-    message = f"Borrower Name: {borrowing.borrower.first_name} " \
-              f"{borrowing.borrower.last_name}\n" \
-              f"You have to pay for borrowing the book '{book.title}'\n"
-
-    if borrowing.actual_return_date:
-        if days_borrowed > 0 and (
-            borrowing.actual_return_date > borrowing.expected_return_date
-        ):
-            overdue_borrowed = (
-                borrowing.actual_return_date - borrowing.expected_return_date
-            ).days
-            borrow_amount = book.daily_fee * days_borrowed + (
-                overdue_borrowed * book.daily_fee * settings.FINE_MULTIPLIER
-            )
-
-            message += f"Amount: ${borrow_amount}\n\n"
-
-        if days_borrowed > 0 and (
-            borrowing.actual_return_date == borrowing.expected_return_date
-        ):
-            borrow_amount = days_borrowed * book.daily_fee
-
-            message += f"Amount: ${borrow_amount}\n\n"
-
-        if days_borrowed > 0 and (
-            borrowing.actual_return_date < borrowing.expected_return_date
-        ):
-            days_actual = (
-                borrowing.actual_return_date - borrowing.borrow_date
-            ).days
-            borrow_amount = days_actual * book.daily_fee
-
-            message += f"Amount: ${borrow_amount}\n\n"
-
-    bot.send_message(chat_id=settings.TELEGRAM_CHAT_ID, text=message)
-
-
 def send_successful_payment_notification(payment_id):
     payment = Payment.objects.get(id=payment_id)
 
